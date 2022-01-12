ac03cfd11366c01618323b338da8113bb3912c2e
Fatimah Aldagdoug <aldagdougfatm@gmail.com>
2022-01-12 15:55:24 +0300

    SignUp

--- app/src/main/java/com/example/thebird/MainActivity.kt
+++ app/src/main/java/com/example/thebird/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.thebird
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.thebird.identity.SignUpScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragmentContainerView,SignUpScreen())
+//            .commit()
+
+
+     }
 }
--- app/src/main/java/com/example/thebird/identity/SignUpScreen.kt
+++ app/src/main/java/com/example/thebird/identity/SignUpScreen.kt
@@ -1,33 +1,26 @@
 package com.example.thebird.identity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.thebird.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +28,54 @@ class SignUpScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_screen, container, false)
+        var v = inflater.inflate(R.layout.fragment_sign_up_screen, container, false)
+        var fnameEditText = v.findViewById<EditText>(R.id.editTextFirstName)
+        var lnameEditText = v.findViewById<EditText>(R.id.editTextLastName)
+        var signUpEmailEditText = v.findViewById<EditText>(R.id.editTextSignUpEmail)
+        var passwordEditText = v.findViewById<EditText>(R.id.editTextTextPassword)
+        var signUpButton = v.findViewById<Button>(R.id.buttonSignUp)
+        var loginTextView=v.findViewById<TextView>(R.id.textViewLogin)
+        var auth = Firebase.auth
+
+
+        loginTextView.setOnClickListener {
+            startActivity(Intent(context,LoginScreen::class.java))
+        }
+
+        signUpButton.setOnClickListener {
+            auth.createUserWithEmailAndPassword(
+                signUpEmailEditText.text.toString(),
+                passwordEditText.text.toString()
+            )
+                .addOnCompleteListener { task ->
+
+                    if (task.isSuccessful) {
+
+                        println("User has been registered successfully with UID " + auth.currentUser?.uid)
+
+                        val u = hashMapOf(
+                            "email" to auth.currentUser?.email,
+                            "firstname" to fnameEditText.text.toString(),
+                            "lastname" to lnameEditText.text.toString()
+
+                        )
+
+                        var db = Firebase.firestore
+
+                        db.collection("users").document(auth.currentUser?.uid.toString())
+                            .set(u)
+
+                    }
+
+
+
+
+
+                }
+
+
+        }
+        return v
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
